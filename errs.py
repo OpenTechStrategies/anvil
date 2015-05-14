0e3f440f19bed5d79cab4d8fa43a5a7bf8c7ce7d
James Vasile <james@jamesvasile.com>
2015-05-13 20:51:36 -0400

    need to be able to raise ParseError

--- errs.py
+++ errs.py
@@ -4,3 +4,10 @@ class ConfigError(Exception):
         self.message = message
         
         sys.stdout.write("\nERROR: " + str(message) + "\n\n")
+
+class ParseError(Exception):
+    def __init__(self, message):
+        self.message = message
+        
+        sys.stdout.write("\nERROR: " + str(message) + "\n\n")
+
