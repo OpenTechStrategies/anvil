26cb94cefd001fd7e9127f58d62b4dcc4abf4049
James Vasile <james@jamesvasile.com>
2015-05-14 18:09:41 -0400

    reorg display stuff

---
+++ display.py
@@ -0,0 +1,11 @@
+class Display():
+    """Display callbacks that get passed in to the implementation to handle display.
+
+    TODO: define the API for Display objects. Or maybe each object has
+    its own API to a degree.
+
+    Right now, we just have command line displays. We might want web
+    display objects at some point.
+
+    """
+
