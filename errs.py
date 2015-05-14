57015bec555ca2a3f2e5893158d00f2dd2ca441c
James Vasile <james@jamesvasile.com>
2015-05-14 19:30:16 -0400

    Make errors a bit easier to copy

--- errs.py
+++ errs.py
@@ -1,13 +1,13 @@
 import sys
-class ConfigError(Exception):
+class GenericException(Exception):
     def __init__(self, message):
         self.message = message
         
         sys.stdout.write("\nERROR: " + str(message) + "\n\n")
 
-class ParseError(Exception):
-    def __init__(self, message):
-        self.message = message
-        
-        sys.stdout.write("\nERROR: " + str(message) + "\n\n")
+class ConfigError(GenericException):
+    pass
+
+class ParseError(GenericException):
+    pass
 
