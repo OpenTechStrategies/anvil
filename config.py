2a05e512b20ff08df9ec3939e9b0836af4498ef1
James Vasile <james@jamesvasile.com>
2015-05-19 21:09:03 -0400

    make config better at finding config.json

--- config.py
+++ config.py
@@ -1,3 +1,22 @@
-import util as u
+import os, sys
 import simplejson as json
-config = json.loads(u.slurp("etc/config.json", split=False))
+import util as u
+from errs import ConfigError
+
+import __main__
+
+path = None
+for p in ["etc/config.json", 
+          "/etc/config.json", 
+          os.path.join(os.path.split(__main__.__file__)[0], "etc/config.json"),
+          os.path.join(os.path.split(__file__)[0], "etc/config.json")]:
+    if os.path.exists(p):
+        path = p
+    else:
+        print p
+
+if path:
+    config = json.loads(u.slurp(path, split=False))
+else:
+    raise ConfigError("Can't find config.json anywhere!")
+
